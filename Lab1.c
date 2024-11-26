e550034924b938e1c7a8bccc6b2dde043717d2c1
Teerayoth 6704062611069 <s6704062611069@email.kmutnb.ac.th>
2024-11-26 15:33:00 +0700

    ข้อ 3 นะครับ

--- Lab1.c
+++ Lab1.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int x;
-	int y;
-	scanf("%d", x);
-	scanf("%d", y);
+	int x , y;
+	scanf("%d", &x);
+	scanf("%d", &y);
 	
 	if (x > y) {	
-		printf("x is greater than y");
-		printf("%d \n" , (x - y));
+		printf("x is greater than y\n");
+		printf("%d" , (x - y));
 	}
 	else {	
-		printf("y is greater than x");
-		printf("%d \n" , (y - x));
+		printf("y is greater than x\n");
+		printf("%d" , (y - x));
 	}
 	
 	return 0;
