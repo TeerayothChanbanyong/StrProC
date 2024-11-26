7fd1eda69a74ae3c0c78f8655629b4203227946e
Teerayoth 6704062611069 <s6704062611069@email.kmutnb.ac.th>
2024-11-26 15:17:05 +0700

    ข้อ 1 นะครับ

---
+++ Lab1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int x = 0;
+	int y = 1;
+	if (x > y) {	
+		printf("x is greater than y");
+	}
+	else {	
+		printf("y is greater than x");
+	}
+	
+	return 0;
+}
+
