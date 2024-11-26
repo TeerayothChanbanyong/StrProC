0e75e6538f2bfeb4e2d37260faf58f248f9e337a
Teerayoth 6704062611069 <s6704062611069@email.kmutnb.ac.th>
2024-11-26 15:19:19 +0700

    ข้อ 2 นะครับ

--- Lab1.c
+++ Lab1.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int x = 0;
-	int y = 1;
+	int x , y;
+	scanf("%d", x);
+	scanf("%d", y);
+	
 	if (x > y) {	
 		printf("x is greater than y");
+		printf("%d \n" , (x - y));
 	}
 	else {	
 		printf("y is greater than x");
+		printf("%d \n" , (y - x));
 	}
 	
 	return 0;
