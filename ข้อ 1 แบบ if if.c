fa1a0b8ffa0a6fa1e8fc5489a8a20b82083e93f8
Teerayoth 6704062611069 <s6704062611069@email.kmutnb.ac.th>
2024-12-03 13:26:14 +0700

    ข้อ 1 แบบ if if
    ตัวเลือกแบบ if if

---
+++ ข้อ 1 แบบ if if.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	int x = 0;
+	int y = 1;
+	if (x > y) {	
+		printf("x is greater than y");
+	}
+	if (y > x){	
+		printf("y is greater than x");
+	}
+ 
+	return 0;
+}
+
